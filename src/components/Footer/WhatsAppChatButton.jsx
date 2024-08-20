b733547da9097a8f9b67d3a2aceb9efbdc4229b1
RajanRajRtr <106738216+RajanRajRtr@users.noreply.github.com>
2024-08-20 17:57:48 +0530

    fixed

--- src/components/Footer/WhatsAppChatButton.jsx
+++ src/components/Footer/WhatsAppChatButton.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import "./style.css"; 
-import whatsapp from"././../../Images/whatsapp.png"
+import React from "react";
+import "./style.css";
+import whatsapp from "././../../Images/whatsapp.png";
 const WhatsAppChatButton = () => {
   const handleClick = () => {
-    window.open('https://wa.me/8124495001', '_blank');
+    window.open("https://wa.me/1234567890", "_blank");
   };
 
   return (
     <div className="whatsapp-chat-button" onClick={handleClick}>
-      <img src={whatsapp} alt="WhatsApp" size={15}/>
+      <img src={whatsapp} alt="WhatsApp" size={15} />
     </div>
   );
 };
